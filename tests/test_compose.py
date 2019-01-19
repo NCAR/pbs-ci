c18fb5d71db5c396ef4d04acedbafe6ecf92fadf
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-19 02:21:13 -0700

    Update tests

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -3,7 +3,11 @@ from __future__ import absolute_import, division, print_function
 import os
 import subprocess
 
+import docker
+
 DOCKER_COMPOSE = os.path.join(os.path.dirname(__file__), "docker-compose.yml")
+
+client = docker.from_env()
 
 
 def test_docker_compose():
@@ -11,3 +15,9 @@ def test_docker_compose():
     proc = subprocess.Popen(cmd)
     ret = proc.wait()
     assert ret == 0
+
+    images = client.images.list()
+    assert len(images) == 1
+
+    containers = client.containers.list()
+    assert len(containers) == 3
