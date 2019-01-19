33cfc252b696b9e5cbdcb2b98666de062b31caef
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-19 06:16:51 -0700

    Fix module import

--- tests/test_script.py
+++ tests/test_script.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import subprocess
+import time
 
 import docker
-import sleep
 
 DOCKER_COMPOSE = os.path.join(os.path.dirname(__file__), "docker-compose.yml")
 
@@ -24,7 +24,7 @@ def test_script():
     ]
     proc_1 = subprocess.Popen(compose_cmd)
     ret_1 = proc_1.wait()
-    sleep(20)
+    time.sleep(20)
     proc_2 = subprocess.Popen(script_cmd)
     ret_2 = proc_2.wait()
     assert ret_1 == 0
