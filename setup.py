b187ecdbe999624770b551bd4bb4f55546744ca8
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-18 19:41:13 -0700

    Add docker to requirements

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
-import versioneer
 from os.path import exists
 
-readme = open("README.rst").read() if exists("README.rst") else ""
+from setuptools import find_packages, setup
 
+import versioneer
+
+readme = open("README.rst").read() if exists("README.rst") else ""
+requirements = ["docker"]
 
 
 setup(
-    name='pbs-ci',
-    description='Continuous integration utility for PBS',
+    name="pbs-ci",
+    description="Continuous integration utility for PBS",
     long_description=readme,
-    maintainer='Anderson Banihirwe',
-    maintainer_email='abanihi@ucar.edu',
-    url='https://github.com/NCAR/pbs-ci',
+    maintainer="Anderson Banihirwe",
+    maintainer_email="abanihi@ucar.edu",
+    url="https://github.com/NCAR/pbs-ci",
     packages=find_packages(),
-    package_dir={'pbs-ci': 'pbs-ci'},
+    package_dir={"pbs-ci": "pbs-ci"},
     include_package_data=True,
-    install_requires=[
-    ],
-    license='Apache-2.0 license',
+    install_requires=requirements,
+    license="Apache 2.0",
     zip_safe=False,
-    keywords='pbs-ci',
+    keywords="pbs-ci",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
